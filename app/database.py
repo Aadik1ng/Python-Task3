1e32f947dd3df0e72b5ea74287b066ee9f04e8cc
Aaditya <aadityaaryan639@gmail.com>
2025-01-28 18:19:41 +0530

    Added DB setup

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models.user import User
+from app.models.attendance import Attendance
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./attendance.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    # Create the tables for the models
+    User.__table__.create(bind=engine, checkfirst=True)
+    Attendance.__table__.create(bind=engine, checkfirst=True)
