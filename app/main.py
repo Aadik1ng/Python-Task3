549b38952692c231a62186109ef7253651deb53f
Aaditya <aadityaaryan639@gmail.com>
2025-01-29 20:47:29 +0530

    Added routes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-# app/main.py
-
 from fastapi import FastAPI
 from app.routes.face_recognition import router as face_recognition_router
-from app.database import init_db
+from app.routes.login import router as login_router  # Import the login router
+from app.services.database import init_db
 from app.services.attendance_csv import CSVHandler
 
 app = FastAPI()
@@ -10,7 +9,12 @@ app = FastAPI()
 # Initialize database
 init_db()
 CSVHandler()
-app.include_router(face_recognition_router, prefix="/api/v1")
+
+# Include the login router for the /api/Login endpoint
+app.include_router(login_router, prefix="/api")
+
+# Include the face recognition router for the /api/v1 endpoint
+app.include_router(face_recognition_router, prefix="/api")
 
 if __name__ == "__main__":
     import uvicorn
