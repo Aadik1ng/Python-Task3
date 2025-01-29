fedbe4d69cf8fac5a91c1417cde1cc67e68a781c
Aaditya <aadityaaryan639@gmail.com>
2025-01-29 19:19:45 +0530

    Added Login/Register models

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,23 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
-
+from pydantic import BaseModel
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True,nullable=False)
+    password = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    
+class LoginRequest(BaseModel):
+    name: str
+    password: str
+    register: bool = False
+
+
+class RegisterRequest(BaseModel):
+    name: str
+    password: str
